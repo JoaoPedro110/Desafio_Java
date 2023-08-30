3ca3ef122c46c452c6962f11a842d3c2003fe82e
João Pedro Magrini <reccojp65@gmail.com>
2023-08-30 08:17:51 -0300

    ok

--- Desafio 3/script.js
+++ Desafio 3/script.js
@@ -1,8 +1,17 @@
-let contador = 0;
-let numero = prompt('Escreva um número inteiro e positivo')
+let contador = 1;
+let numero = parseInt(prompt("Digite um numero"));
+let msg = "Contagem";
 
-
-while (contador < numero) {
-    contador++;
-    console.log(contador);
+if (isNaN(numero) || numero <= 0) {
+    alert("Numero digitado invalido");
+} else {
+    while (contador <= numero){
+        msg = msg + '  ' + contador;
+        contador++;
+    }
+    msg = msg + ' Acabou!';
+    alert(msg);
 }
+
+
+
